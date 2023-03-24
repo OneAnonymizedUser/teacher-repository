d41134438314f880e2ced7f9a1f9cdcc399cbbb5
OneAnonymizedUser <md201820181@gmail.com>
2023-03-24 18:48:39 +0100

    teacher implementation

---
+++ src/main/java/Adder.java
@@ -0,0 +1,11 @@
+public class Adder {
+
+    public static int increment(int x) {
+        // STUDENT throw new RuntimeException("Not implemented");
+        // BEGIN STRIP
+        return x + 1;
+        // END STRIP
+    }
+
+}
+
